d009c614613a053958c7966b48b158711f3feb00
SamRock294 <sakaethrambusiness@gmail.com>
2024-10-28 23:44:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+/* Onclick Response */
+
+document.getElementById("DirectMeta").onclick = function() {
+    window.location.href = "file:///C:/Users/sakae/OneDrive/Documents/Projects/Arkin%20MetaLib/Home%20Page/Meta%20Data/index.html";
+  };
