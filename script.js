deab7d1b57be1f640e0167ff1ea80e71f1845507
SamRock294 <sakaethrambusiness@gmail.com>
2024-11-13 12:34:51 +0530

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /* Onclick Response */
 
 document.getElementById("DirectMeta").onclick = function() {
-    window.location.href = "file:///C:/Users/sakae/OneDrive/Documents/Projects/Arkin%20MetaLib/Home%20Page/Meta%20Data/index.html";
+    window.location.href = "https://samrock294.github.io/MetaLib/";
   };
